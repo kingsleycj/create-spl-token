8405e02ef95348e493908bdbec393641ad82eed9
Kingsley Nweke <primekings.kc@gmail.com>
2024-02-17 09:51:52 +0000

    created connection, generated new keypair for "fromWallet && toWallet", performed an airdrop into the fromWallet, initiated creation of a mint account, and finally returned pubkey of token account from the fromWallet

--- index.js
+++ index.js
@@ -2,13 +2,30 @@ import { clusterApiUrl, Connection, Keypair, LAMPORTS_PER_SOL } from '@solana/we
 import { createMint, getOrCreateAssociatedTokenAccount, mintTo, transfer } from '@solana/spl-token';
 
 (async () => {
-    // Step 1: Connect to cluster and generate a new Keypair
+// Step 1: Connect to cluster and generate two new Keypairs
+const connection = new Connection(clusterApiUrl('devnet'), 'confirmed');
+
+const fromWallet = Keypair.generate();
+const toWallet = Keypair.generate();
+
     
 
-    // Step 2: Airdrop SOL into your from wallet
+// Step 2: Airdrop SOL into your from wallet
+const fromAirdropSignature = await connection.requestAirdrop(fromWallet.publicKey, LAMPORTS_PER_SOL);
+// Wait for airdrop confirmation
+await connection.confirmTransaction(fromAirdropSignature, { commitment: "confirmed" });
+
     
-    // Step 3: Create new token mint and get the token account of the fromWallet address
-    //If the token account does not exist, create it
+// Step 3: Create new token mint and get the token account of the fromWallet address
+//If the token account does not exist, create it
+const mint = await createMint(connection, fromWallet, fromWallet.publicKey, null, 9);
+const fromTokenAccount = await getOrCreateAssociatedTokenAccount(
+        connection,
+        fromWallet,
+        mint,
+        fromWallet.publicKey
+)
+
     
     
     //Step 4: Mint a new token to the from account
